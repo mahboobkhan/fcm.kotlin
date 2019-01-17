fb124d0bdc51169b6554ab1e95950a2918c9f7d9
mahboobkhan0012 <mahboobk522@gmail.com>
2019-01-17 11:10:09 +0500

    updated

--- app/src/main/java/fcm/firebaserepo/com/firebase/MainActivity.kt
+++ app/src/main/java/fcm/firebaserepo/com/firebase/MainActivity.kt
@@ -2,11 +2,23 @@ package fcm.firebaserepo.com.firebase
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
+
+    fun subscribeTopic(topic:String){
+        FirebaseMessaging.getInstance().subscribeToTopic(topic)
+    }
+
+
 }
---
+++ firebasex/src/main/java/fcm/firebaserepo/com/firebasex/utils/SubscribeTopic.kt
@@ -0,0 +1,14 @@
+package fcm.firebaserepo.com.firebasex.utils
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
+
+class SubscribeTopic {
+
+    fun subscribaTopics(topic:String) {
+        FirebaseMessaging.getInstance().subscribeToTopic(topic)
+    }
+    fun logi(tag:String,message:String){
+        Log.d(tag,message)
+    }
+}
